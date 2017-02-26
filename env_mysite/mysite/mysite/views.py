3fae45a33e93d1f3ec9ad1fd5b2ee23cee92c9c0
Navindren Baskaran <navin25@hotmail.com>
2017-02-26 17:01:37 +0800

    Change current_datetime function to use render

--- env_mysite/mysite/mysite/views.py
+++ env_mysite/mysite/mysite/views.py
@@ -1,6 +1,4 @@
-from django.template.loader import get_template
-from django.template import Context
-from django.http import HttpResponse
+from django.shortcuts import render
 import datetime
 
 def hello(request):
@@ -13,9 +11,7 @@ def creator(request):
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    t = get_template('current_datetime.html')
-    html = t.render(Context({'current_date': now}))
-    return HttpResponse(html)
+    return render(request, "current_datetime.html", {'current_datetime': now})
 
 def current_datetime_ ():
     now = datetime.datetime.now()
